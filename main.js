49e6019835c316875434bf53cdf83b9ae32310cb
SvetlanaKhvan <111428745+SvetlanaKhvan@users.noreply.github.com>
2022-10-24 22:45:55 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,42 @@
+//calculating the area of a circle (first problem)
+
+let PI = 3.14;
+
+function circle(radius) {
+    result = PI*radius*radius
+    return result
+}
+
+let A=function(radius) {
+    result = PI*radius*radius
+    return result
+}
+console.log('Area =', A(7.5).toFixed(3));
+
+
+// checking if the number is even or odd (second problem)
+
+let number=1522;
+
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
+
+//checking if the number is positive or negative (third problem)
+
+let figure=-757
+
+if (figure>0) {
+    console.log(figure + " is positive. ");
+} else {
+    console.log(figure + " is negative. ");
+}
+
+
+
+
+
+ 
